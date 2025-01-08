0249a48a66b54f545f5b665a388028d49c128e15
Lirosk <kirill_crysis@bk.ru>
2025-01-08 12:13:51 +0300

    Delegate auth logic to service

--- src/main/java/lirosk/springrestauth/controllers/AuthController.java
+++ src/main/java/lirosk/springrestauth/controllers/AuthController.java
@@ -1,55 +1,34 @@
 package lirosk.springrestauth.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import lirosk.springrestauth.dto.AuthRequest;
 import lirosk.springrestauth.dto.LoginResponse;
 import lirosk.springrestauth.dto.RegisterRequest;
 import lirosk.springrestauth.dto.UserInfo;
-import lirosk.springrestauth.models.CustomUser;
-import lirosk.springrestauth.repositories.CustomUserRepository;
-import lirosk.springrestauth.security.jwt.JwtEncoder;
+import lirosk.springrestauth.services.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/auth")
 public class AuthController {
-    private final JwtEncoder jwtEncoder;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final CustomUserRepository customUserRepository;
+    private final AuthService authService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public UserInfo register(@RequestBody @Validated RegisterRequest authRequest) {
-        if (customUserRepository.findByUsername(authRequest.getUsername()) != null) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
-        }
-        return UserInfo.of(customUserRepository.save(authRequest.toUser(passwordEncoder)));
+        return authService.attemptRegister(authRequest);
     }
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Validated AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        CustomUser customUser = (CustomUser) authentication.getPrincipal();
-
-        String token = jwtEncoder.encode(customUser);
-        return new LoginResponse(token);
+        return authService.attemptLogin(authRequest);
     }
 }
---
+++ src/main/java/lirosk/springrestauth/services/AuthService.java
@@ -0,0 +1,46 @@
+package lirosk.springrestauth.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import lirosk.springrestauth.dto.AuthRequest;
+import lirosk.springrestauth.dto.LoginResponse;
+import lirosk.springrestauth.dto.RegisterRequest;
+import lirosk.springrestauth.dto.UserInfo;
+import lirosk.springrestauth.models.CustomUser;
+import lirosk.springrestauth.repositories.CustomUserRepository;
+import lirosk.springrestauth.security.jwt.JwtEncoder;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final JwtEncoder jwtEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserRepository customUserRepository;
+
+    public UserInfo attemptRegister(RegisterRequest authRequest) {
+        if (customUserRepository.findByUsername(authRequest.getUsername()) != null) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
+        }
+        return UserInfo.of(customUserRepository.save(authRequest.toUser(passwordEncoder)));
+    }
+
+    public LoginResponse attemptLogin(AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+
+        String token = jwtEncoder.encode(customUser);
+        return new LoginResponse(token);
+    }
+}
